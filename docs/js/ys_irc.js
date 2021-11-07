9404927879ab67d38ceb24cc4fb8437d08409ec4
loktindyi <1282071139@qq.com>
2021-11-07 21:23:29 +0800

    Signed-off-by: loktindyi <1282071139@qq.com>

--- docs/js/ys_irc.js
+++ docs/js/ys_irc.js
@@ -18,6 +18,7 @@ const world = {
         bow: '弓'
     },
     rv: {
+        arv: 'ARV',
         v10: '1.0',
         v11: '1.1',
         v12: '1.2',
@@ -28,78 +29,99 @@ const world = {
         v20: '2.0',
         v21: '2.1',
         v22: '2.2',
-        v66: '6.6',
+        v66: '6.6'
     }
 }
 // say
-const say = msg => {
-    $('#toast').html(msg)
-    $('#toast').css('display', 'inline')
-    setTimeout(() => { $('#toast').css('display', 'none') }, Math.min(msg.length / 4, 10) * 1000)
-}
+const vToast = new Vue({
+    el: '#toast',
+    data: {
+        message: '',
+        show: false
+    },
+    methods: {
+        say: function (msg) {
+            this.message = msg
+            this.show = true
+            setTimeout(() => { this.show = false }, Math.min(msg.length / 4, 10) * 1000)
+        }
+    },
+})
+
 // 过滤掉数据中不可用信息
-let imgDataAvailable = {}
-Object.entries(imgData).forEach(v => { if (v[1].Available) imgDataAvailable[v[0]] = v[1] })
+let availableImgData = {}
+Object.entries(imgData).forEach(v => { if (v[1].Available) availableImgData[v[0]] = v[1] })
 // 根据过滤器更新页面
 const getCharacter = (ele, wea, rev) => {
     let displayCharacterList = []
-    Object.entries(imgDataAvailable).filter(c => ele.includes(c[1].Element) && wea.includes(c[1].Weapon) && rev.includes(c[1].ReleaseVersion)).forEach(e => { displayCharacterList.push(e[0]) })
-    const noDisplayCharacterList = Object.keys(imgDataAvailable).filter(x => !displayCharacterList.some(v => x == v))
+    Object.entries(availableImgData).filter(c => ele.includes(c[1].Element) && wea.includes(c[1].Weapon) && rev.includes(c[1].ReleaseVersion)).forEach(e => { displayCharacterList.push(e[0]) })
+    const noDisplayCharacterList = Object.keys(availableImgData).filter(x => !displayCharacterList.some(v => x == v))
     displayCharacterList.forEach(d => { $(`#${d}-li`).css('display', 'block') })
     noDisplayCharacterList.forEach(nd => { $(`#${nd}-li`).css('display', 'none') })
     return [displayCharacterList, noDisplayCharacterList]
 }
 // 当前页全选
-const selectAll = (bool) => { $(`.chac-navbar li${bool ? '[style*="block"]' : ''}`).each(function () { $(this).find('input')[0].checked = bool }) }
+const selectAll = (bool) => { $(`.chac-li${bool ? '[style*="block"]' : ''}`).each(function () { $(this).find('input')[0].checked = bool }) }
 // 清除过滤器 不取消选择
 const resetFilter = () => {
     // selectAll(false);
-    $('input[name="element"]:checked, input[name="weapon"]:checked, input[name="releaseVer"]:checked').each(function () { this.checked = false })
+    $('.elem:checked, .weap:checked, .rev:checked').each(function () { this.checked = false })
     getCharacter(Object.keys(world.el), Object.keys(world.wl), Object.values(world.rv))
 }
 // 导出
 const exportToClip = () => {
-    let pscode = `$wClient = New-Object System.Net.WebClient
-$saveDir = "$env:USERPROFILE\\Desktop\\GenshinImages\\"
-if (-not (Test-Path $saveDir)) { $null = New-Item -ItemType Directory $saveDir }\n`
+    let pscode = `$w = New-Object System.Net.WebClient
+$s = '$env:USERPROFILE\\Desktop\\GenshinImages\\'
+if (-not (Test-Path $s)) { $null = New-Item -ItemType Directory $s }\n`
     if ($('.chac-label input:checked').length) {
         let chas = []
         $('.chac-label input:checked').each(function () {
             let id = this.id
-            let cha = imgDataAvailable[id]
+            let cha = availableImgData[id]
             chas.push(' ' + cha.Name)
-            pscode += `$wClient.DownloadFile('${imgDataAvailable[id].Icon}', $saveDir + '${id}[Icon].png')
-${cha.Card !== null ? `$wClient.DownloadFile('${cha.Card}', $saveDir + '${id}[Card].png')` : ''}
-${cha.Portrait !== null ? `$wClient.DownloadFile('${cha.Portrait}', $saveDir + '${id}[Portrait].png')` : ''}
-${cha.Wish !== null ? `$wClient.DownloadFile('${cha.Wish}', $saveDir + '${id}[Wish].png')` : ''}
-${cha.Avatar !== null ? `$wClient.DownloadFile('${cha.Avatar}', $saveDir + '${id}[Avatar].png')` : ''}\n`
+            pscode += `$w.DownloadFile('${availableImgData[id].Icon}', $s + '${id}[Icon].png')
+${cha.Card !== null ? `$w.DownloadFile('${cha.Card}', $s + '${id}[Card].png')` : ''}
+${cha.Portrait !== null ? `$w.DownloadFile('${cha.Portrait}', $s + '${id}[Portrait].png')` : ''}
+${cha.Wish !== null ? `$w.DownloadFile('${cha.Wish}', $s + '${id}[Wish].png')` : ''}
+${cha.Avatar !== null ? `$w.DownloadFile('${cha.Avatar}', $s + '${id}[Avatar].png')` : ''}\n`
         })
         pscode += 'explorer.exe $saveDir'
         if (confirm(`共 ${chas.length} 个角色：${chas}`)) {
-            say(`导出到剪切板 共 ${chas.length} 个角色：${chas.length > 7 ? chas.slice(0, 7) + ' 等...' : chas}`)
+            vToast.say(`导出到剪切板 共 ${chas.length} 个角色：${chas.length > 7 ? chas.slice(0, 7) + ' 等...' : chas}`)
             navigator.clipboard.writeText(pscode)
-        } else say('导出被取消')
-    } else say('还未选择角色')
+        } else vToast.say('导出被取消')
+    } else vToast.say('还未选择角色')
 }
 // 初始化过滤器，读取所有角色并渲染
 const loadAllCharacters = () => {
-    Object.entries(world.el).forEach(e => { $('ul.elem-navbar').append($(`<li><label class="elem-label ${e[0]}-lab" for="${e[0]}"><span>${e[1]}</span><img src="./img/elements/${e[0]}.png" alt="${e[1]}"></label></li>`)) })
-    Object.entries(world.wl).forEach(w => { $('ul.weap-navbar').append($(`<li><label class="weap-label ${w[0]}-lab" for="${w[0]}"><span>${w[1]}</span><img src="./img/weapons/${w[0]}.png" alt="${w[1]}"></label></li>`)) })
-    Object.entries(world.rv).forEach(v => { $('ul.rev-navbar').append($(`<li><label class="rev-label ${v[0]}-lab" for="${v[0]}"><span>${v[1]}</span></label></li>`)) })
-    Object.entries(imgDataAvailable).forEach(c => { $('ul.chac-navbar').append($(`<li id="${c[0]}-li" style="display: block;"><label class="chac-label" for="${c[0]}"><input type="checkbox" id="${c[0]}"><img src="./img/icon/${c[1].IconName}.png" alt="${c[1].Name}"><span>${c[1].Name}</span></label></li>`)) })
+    const vFilters = new Vue({
+        el: '#filter',
+        data: {
+            elementList: world.el,
+            weaponList: world.wl,
+            releaseVerList: world.rv
+        }
+    })
+    const vCharacters = new Vue({
+        el: '.chac-list',
+        data: {
+            characterList: availableImgData
+        }
+    })
+    $('#arv').attr('checked', true)
     $('#FuraVetind-li').html(`<label class="fura-label"><input type="checkbox" style="display: none;"><img src="./img/icon/FuraVetind.png" alt="风风轮舞"><span>风风轮舞</span></label>`)
 }
 $('ul.chac-navbar').ready(() => {
     loadAllCharacters()
-    say(`这个工具应在电脑上使用，手机只能看看...`)
+    vToast.say(`这个工具应在电脑上使用，手机只能看看...`)
 })
 // 读取过滤器
 const loadFilter = () => {
-    let eleList = [], weaList = [], revList = []
-    $('#element input:checked').each(function () { eleList.push(this.id) })
-    $('#weapon input:checked').each(function () { weaList.push(this.id) })
-    $('#releaseVer input:checked').each(function () { revList.push(this.id) })
-    revList = revList.map(v => v[1] + '.' + v[2])
-    return [eleList.length ? eleList : Object.keys(world.el), weaList.length ? weaList : Object.keys(world.wl), revList.length ? revList : Object.values(world.rv)]
+    let eleList = [], weaList = [], rev = 'arv'
+    $('.elem:checked').each(function () { eleList.push(this.id) })
+    $('.weap:checked').each(function () { weaList.push(this.id) })
+    $('.rev:checked').each(function () { rev = this.id })
+    rev = rev[1] + '.' + rev[2]
+    return [eleList.length ? eleList : Object.keys(world.el), weaList.length ? weaList : Object.keys(world.wl), rev === 'r.v' ? Object.values(world.rv) : [rev]]
 }
-world.watch('#element input, #weapon input, #releaseVer input', 'change', () => { getCharacter(...loadFilter()) })
+world.watch('.elem, .weap, .rev', 'change', () => { getCharacter(...loadFilter()) })
